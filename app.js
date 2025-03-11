7af799afa0f574c2b648871c0f2234e62600248b
huslen0212 <huslen.battogtoh@yahoo.com>
2025-03-11 19:01:43 +0800

    huslen5

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
+
 // Requiring module
-const express = require('express');
+const express = require('express'); 
+const path = require("path");
+const port = 3000; 
 
 // Creating express object
 const app = express();
-
+app.use("/public", express.static("public"));
 // Handling GET request
 app.get('/', (req, res) => { 
-    res.send('A simple Node App is '
-        + 'running on this server') 
-    res.end() 
+    res.sendFile(path.join(__dirname, "/src/index.html"));
 }) 
 
 // Port Number
 const PORT = process.env.PORT ||5000;
 
 // Server Setup
-app.listen(PORT,console.log(
-  `Server started on port ${PORT}`));
+app.listen(port, () => console.log(`App listening on port http://localhost:${port}`))
